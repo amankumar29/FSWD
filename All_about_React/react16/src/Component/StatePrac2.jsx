d916e922cdfd4a2328f6fd0dbf2653259b270b29
codewithhappy <mannukumaraman@gmail.com>
2021-11-30 11:24:51 +0530

    ok

--- All_about_React/react16/src/Component/StatePrac2.jsx
+++ All_about_React/react16/src/Component/StatePrac2.jsx
@@ -3,18 +3,36 @@ class StatePrac2 extends React.Component{
     state={
         qty:0,
     }
-    change=()=>{
+    add=()=>{
+        if(this.state.qty<10){
         this.setState({qty:this.state.qty+1})
         console.log(this.state.qty);
-        console.log("check change");
+        console.log("check add");
+        }
+        else{
+            alert("reached maximum qty")
+        }
+        
     }
+    sub=()=>{
+        if(this.state.qty>0){
+            this.setState({qty:this.state.qty-1})
+        console.log(this.state.qty)
+        console.log("check sub")
+        }
+        else{
+            alert("atleast 1 qty required")
+        }
+        
+    }
+    
     render(){
         console.log("check render")
         return(
             <>
             <h6>{this.state.qty}</h6>
-            <button onClick={this.change}>Add</button>
-            <button onclick={this.chnage}>Sub</button>
+            <button onClick={this.add}>Add</button>
+            <button onClick={this.sub}>Sub</button>
 
             <h1> Hello..</h1>
             </>
